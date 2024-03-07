ba87d0dfc736c0fa82d7b71e476960f08f5e274a
ganpeng <190875565@qq.com>
2024-03-07 15:41:57 +0800

    first commit

---
+++ vue.config.js
@@ -0,0 +1,8 @@
+const { defineConfig } = require("@vue/cli-service");
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+module.exports = defineConfig({
+  transpileDependencies: true,
+  configureWebpack: {
+    plugins: [new BundleAnalyzerPlugin()],
+  },
+});
